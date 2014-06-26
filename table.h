8d8d3f685e0858da7489df6b9113fba26f775ffc
Tim Panton <tpanton@voxeolabs.com>
2014-06-26 11:45:09 -0700

    Yopet initial check in

---
+++ table.h
@@ -0,0 +1,10 @@
+      <table>
+      <tr> <td colspan="2"><img src="images/petbanner.png"/></td> </tr>
+      <tr><td rowspan="3"><div id="videoContainer" class="ui-video-container">
+      <div id="remoteVideoContainer" class="ui-video-remote-container"></div>
+      <div id="localVideoContainer" class="ui-video-local-container"></div>
+      </td><td><img src="images/sidebar1.png"/></td></tr>
+      <tr><td><img src="images/sidebar2.png"/></td></tr>
+      <tr><td><img src="images/sidebar3.png"/></td></tr>
+      </table>
+	<script>
